d832d3344376ce379386c14846a77a8897f891da
Pan T. <107498260+PanTheRandomness@users.noreply.github.com>
2025-04-07 14:07:53 +0300

    Correction: Changed WorldRepository.java from class file to interface file

--- src/main/java/com/example/application/data/WorldRepository.java
+++ src/main/java/com/example/application/data/WorldRepository.java
@@ -2,5 +2,5 @@ package com.example.application.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public class WorldRepository /*extends JpaRepository<World, Long> */{
+public interface WorldRepository extends JpaRepository<World, Long> {
 }
