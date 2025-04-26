655856ad3c04f1eaa016cb0280fc55d1722178f8
Pan T. <107498260+PanTheRandomness@users.noreply.github.com>
2025-04-26 13:22:20 +0300

    forgot to push this

--- src/main/java/com/example/application/data/repositories/EventTypeRepository.java
+++ src/main/java/com/example/application/data/repositories/EventTypeRepository.java
@@ -10,10 +10,6 @@ import java.util.List;
 
 public interface EventTypeRepository extends JpaRepository<EventType, Long> {
 
-    // TODO: FIX these! They don't work
-    @Query("SELECT e.type FROM Event e WHERE e.campaign IN :campaigns")
-    List<EventType> findEventTypesByCampaigns(@Param("campaigns") List<Campaign> campaigns);
-
     @Query("SELECT e.type FROM Event e WHERE e.campaign = :campaign")
     List<EventType> findEventTypesByCampaign(@Param("campaign") Campaign campaign);
 }
